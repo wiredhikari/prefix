9e9be1bd393f43f2fca46787285677afe99b4678
Atharva <atharvaamritkar@protonmail.com>
2022-05-25 14:54:08 +0530

    testing

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -376,7 +376,7 @@ bootstrap_setup() {
 		fi
 		[[ -f ${ROOT}/etc/resolv.conf ]] || ln -s {,"${ROOT}"}/etc/resolv.conf
 		[[ -f ${ROOT}/etc/hosts ]] || cp {,"${ROOT}"}/etc/hosts
-		local profile_linux=default/linux/ARCH/
+		local profile_linux=default/linux/ARCH/20.0/prefix/$(profile-kernel)
 	else
 		local profile_linux=prefix/linux/ARCH
 	fi
@@ -424,7 +424,7 @@ bootstrap_setup() {
 			;;
 		riscv64-pc-linux-gnu)
 			profile=${profile_linux/ARCH/riscv}	
-			profile=${profile/20.0}
+			profile=${profile/20.0/}
 			;;	
 		x86_64-pc-linux-gnu)
 			profile=${profile_linux/ARCH/amd64}
