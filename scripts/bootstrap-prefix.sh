f259227076f336e6e274ebfd263ac2c587f1dc62
Atharva <atharvaamritkar@protonmail.com>
2022-07-01 15:21:29 +0530

    using gz2

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -622,9 +622,9 @@ bootstrap_tree() {
 	is-rap && LATEST_TREE_YES=1
 	local PV="20220616"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
-		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
+		do_tree "${SNAPSHOT_URL}" portage-latest.tar.gz
 	else
-	do_tree https://github.com/wiredhikari/portage/releases/tags/prefix-overlay.tar.bz2
+	do_tree https://github.com/wiredhikari/portage/archive/refs/tags/portage-latest.tar.gz
 	fi
 	local ret=$?
 	if [[ -n ${TREE_FROM_SRC} ]]; then
